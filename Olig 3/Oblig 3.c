3fccaf22ea24c9c762b07443e637288e6c9c3b39
gjermundhp02 <gjermund.pedersen@gmail.com>
2023-10-16 19:07:37 +0200

    Added comments

--- Olig 3/Oblig 3.c
+++ Olig 3/Oblig 3.c
@@ -2,10 +2,10 @@
  * @file Oblig.c
  * @author Gjermund H. Pedersen
  */
-#include <stdbool.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include <stdbool.h> // bool
+#include <stdio.h> // scanf, printf, getChar, gets
+#include <ctype.h> // idalpha, isdigit
+#include <string.h> // strlen
 
 const int STRLEN = 80; ///< Max.tekstlengde.
 const int ANTINT = 20; ///< Lengden på int-array.
@@ -18,6 +18,8 @@ bool sjekkTekst(const char tekst[]);
 void skrivMeny();
 
 /**
+ * Kopiert fra oppgaven
+ * 
  * Hovedprogrammet:
  */
 int main()
@@ -35,14 +37,14 @@ int main()
             fyllArray(tallene, ANTINT);
             break;
         case 'A':
-            printf("\nAntall i arrayen i intervallet 0-2000: %i\n",
+            printf("\nAntall i arrayen i intervallet 0-2000: %i\n\n",
                    antallIArray(tallene, ANTINT, 0, 2000));
             break;
         case 'L':
             lesTekst("Postnummer og -sted", teksten);
             break;
         case 'S':
-            printf("\nTeksten er%s et gyldig postnr og -sted.\n",
+            printf("\nTeksten er%s et gyldig postnr og -sted.\n\n",
                    ((!sjekkTekst(teksten)) ? " IKKE" : ""));
             break;
         default:
@@ -61,13 +63,15 @@ int main()
  * @param n    - Lengden av tall
  * @param min  - Minste verdi som skal telles (Inklusiv)
  * @param max  - Største verdi som skal telles (Eksklusiv)
+ * 
+ * @return Antall elementer i arrayet som er mellom de to gitte verdiene
  */
 int antallIArray(int tall[], const int n, const int min, const int max)
 {
-    int antallIArray = 0;
-    for (int i = 0; i < n; i++)
+    int antallIArray = 0; // Antall tall som er i intervalet
+    for (int i = 0; i < n; i++) // Går gjennom alle tallene
     {
-        if (min <= tall[i] && tall[i] < max)
+        if (min <= tall[i] && tall[i] < max) // Sjekker om det er innenfor
         {
             antallIArray++;
         }
@@ -83,7 +87,7 @@ int antallIArray(int tall[], const int n, const int min, const int max)
  */
 void fyllArray(int tall[], const int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // Leser et input for hver index i arrayet
     {
         printf("Neste tall: ");
         scanf("%i", &tall[i]);
@@ -92,6 +96,7 @@ void fyllArray(int tall[], const int n)
 
 /**
  *  Leser og returnerer ett (upcaset) tegn.
+ *  Kopiert fra oppgaven
  *
  *  @return  Ett (upcaset) tegn.
  */
@@ -106,6 +111,7 @@ char lesKommando()
 
 /**
  *  Leser og returnerer et tall mellom to gitte grenser.
+ *  Kopiert fra oppgaven
  *
  *  @param   tekst  - Ledetekst til brukeren når ber om input/et tall
  *  @param   min    - Minimum for innlest og godtatt tallverdi
@@ -127,6 +133,7 @@ int lesTall(const char tekst[], const int min, const int max)
 
 /**
  *  Leser tekst inn i medsendt char-array/tekst/string.
+ *  Kopiert fra oppgaven
  *
  *  @param   ledetekst  - Ledetekst som utskrift til brukeren om hva lese inn
  *  @param   tekst      - char-array som fylles med innlest tekst
@@ -137,33 +144,44 @@ void lesTekst(const char ledetekst[], char tekst[])
     gets(tekst); //  Oppdaterer ORIGINALEN/aktuell parameter.
 }
 
+/**
+ * Sjekker om teksten som blir sendt inn er gyldig postkode og adresse
+ * 
+ * @param tekst - Teksten som skal sjekkes 
+ * 
+ * @return Om teksten er gyldig postkode 
+ */
 bool sjekkTekst(const char tekst[])
 {
     const int tekstLengde = strlen(tekst);
-    if (tekstLengde<5)return false;
+    if (tekstLengde<5)return false; // Sjekker om teksten er under 5 karakterer. 
+                                    // Den er alltid ugyldig da
     
-    int numTall = 4;
-    for (int i = 0; i < tekstLengde; i++)
+    int indexSisteTall = 4; // Lagrer indexen til det siste tallet i postkoden
+    for (int i = 0; i < tekstLengde; i++) // Lopper lengden av teksten
     {
-        printf("%c", tekst[i]);
         if (isdigit(tekst[i]))
         {
-            if (i>1)
+            if (i>1) // Hvis den er på det tredje eller fjerde tallet
             {
-                if (tekst[i+1]==' ')
-                {
-                    numTall=i;
+                if (tekst[i+1]==' ') // Hvis neste karakter er ' ' er slutten av
+                {                    // postkoden funnet
+                    indexSisteTall=i;
                 }
             }
         }
-        else if (i>numTall)
+        else if (i>indexSisteTall)
         {
-            if (!isalpha(tekst[i]) && !tekst[i] == ' ' && !tekst[i] == '.' && !tekst[i] == '-')
+            if (!isalpha(tekst[i]) && // Hvis karakteren ikke er a-z, '.', 
+                !tekst[i] == ' ' &&   // ' ' eller '-' er ikke teksten gyldig
+                !tekst[i] == '.' && 
+                !tekst[i] == '-')
             {
                 return false;
             }
         }
-        else{
+        else{ // Hvis karakteren ikke er et tall eller løkken er forbi der
+              // slutten av postkoden burde vært er teksten ikke gyldig
             return false;
         }
     }
